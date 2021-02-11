413a8085ba0464e29bc4f2827600c012251a8087
Philkapilka <54175032+Philkapilka@users.noreply.github.com>
2021-02-11 17:55:08 +0300

    Delete some comments, add new modal window style, add a windows close function on a "submit" event

--- app.js
+++ app.js
@@ -25,10 +25,8 @@ submit.addEventListener('click', (e) => {
 
   const blog = document.createElement("DIV");
   const blogContainer = document.createElement("DIV");
-  // const titleText = document.createElement("P");
   const aboutText = document.createElement("P");
-  // titleText.innerHTML = title.value;
-  // titleText.classList.add('title-text');
+
   blogContainer.classList.add('text-container');
   aboutText.innerHTML = about.value;
   aboutText.classList.add('about-text');
@@ -43,38 +41,6 @@ submit.addEventListener('click', (e) => {
   blogContainer.append(aboutText);
   console.log('Объект создался');
   console.log(title.value);
-  
-  // const blog = document.createElement('DIV');
-  // blog.classList.add('news-col');
-  // blog.textContent = "Lorem ipsum dolor sit amet";
-  // blog.style.display = 'block';
-  // row.style.display = 'block';
-  // row.style.margin = '10px';
-  // row.prepend(blog);
-  // row.removeChild(some);
-  // console.log('Объект создался');
+
+  modal.style.display = "none";
 });
-
-// let button = document.querySelector('button');
-// let list = document.querySelector('ul');
-// button.addEventListener('click', () => {
-//     const li = document.createElement('li');
-//     li.textContent = prompt('Введите название нового ToDo');
-//     list.append(li);
-// });
-
-// list.addEventListener('click', (e) => {
-//     if (e.target.tagName === 'LI') {
-//         e.target.remove();
-//     }
-// })
-
-// let button = document.querySelector('button');
-// let div = document.querySelector('.container');
-
-
-// button.addEventListener('click', () => {
-//   const li = document.createElement('li');
-//   li.textContent = 'Введите название нового ToDo';
-//   div.prepend(li);
-// });
